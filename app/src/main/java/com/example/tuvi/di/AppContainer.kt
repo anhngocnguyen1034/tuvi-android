b27b23d594a484117382afeeaded03245b9a0c4a
nguyenngocanh <nguyenanhcry@gmail.com>
2026-04-29 23:44:17 +0700

    update : theme light/dark

--- app/src/main/java/com/example/tuvi/di/AppContainer.kt
+++ app/src/main/java/com/example/tuvi/di/AppContainer.kt
@@ -22,7 +22,7 @@ import retrofit2.Retrofit
 
 object AppContainer {
 
-    private const val BASE_URL = "http://192.168.1.15:8000"
+    private const val BASE_URL = "http://192.168.0.100:8000"
 //    private const val BASE_URL = "http://192.168.0.102:8000"
 
     lateinit var app: android.app.Application
